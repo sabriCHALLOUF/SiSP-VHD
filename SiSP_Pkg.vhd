e790e62a91bdfdcd4e54ab000309da4ad5bcc126
Sabri <36789247+sabriCHALLOUF@users.noreply.github.com>
2022-09-02 11:15:50 +0100

    Update SiSP_Pkg.vhd

--- SiSP_Pkg.vhd
+++ SiSP_Pkg.vhd
@@ -2,7 +2,9 @@
 -- Author        : Sabri Challouf               --
 -- Creation date : 23 - 10 - 2016               --
 --                                              --
--- File description:                            --
+-- File description: This file contains some 	--
+-- declarations used on the hardware 		--
+-- implemntation of the SiSP                    --
 --------------------------------------------------
 
 LIBRARY IEEE;
@@ -17,34 +19,28 @@ PACKAGE SiSP_Tools IS
 	SUBTYPE SiSP_Vector is std_logic_vector(N-1 downto 0) ;
 	TYPE  std_logic_array_vector IS ARRAY ( NATURAL RANGE <>) OF SiSP_Vector;
 	
-	CONSTANT 		  UnInit		: 	SiSP_Vector:=(others => 'U');
-	CONSTANT 		  Unknown		: 	SiSP_Vector:=(others => 'X');
-	CONSTANT 		  Full_0		: 	SiSP_Vector:=(others => '0');
-	CONSTANT 		  Full_1		: 	SiSP_Vector:=(others => '1');
-	--CONSTANT				CMP_VAL	:	SiSP_Vector:=
-		
+	CONSTANT	UnInit	:	SiSP_Vector:=(others => 'U');
+	CONSTANT 	Unknown	: 	SiSP_Vector:=(others => 'X');
+	CONSTANT 	Full_0	: 	SiSP_Vector:=(others => '0');
+	CONSTANT 	Full_1	: 	SiSP_Vector:=(others => '1');
 	
 	FUNCTION resolved_vector ( sources : std_logic_array_vector) RETURN SiSP_Vector;
-	
-	
 END PACKAGE SiSP_Tools;
 
 
 
 --Package Implementation
 PACKAGE BODY SiSP_Tools IS
-
-		FUNCTION resolved_vector ( sources : std_logic_array_vector ) RETURN SiSP_Vector IS
-		  VARIABLE result : SiSP_Vector; -- weakest state default
-		BEGIN
-		  IF (sources'LENGTH = 1) THEN RETURN sources(sources'LOW);
-			 ELSE
-				FOR i IN sources'RANGE LOOP
-					result := std_logic_vector(to_unsigned(2 ** (i - (sources'LENGTH + 1)),N) * unsigned(sources(i)));
-				END LOOP;
-				--result := result + sclk / (2 ** (sources'LENGTH + 1));
-		  END IF;
-		  RETURN result;
-		END resolved_vector;
-
+	FUNCTION resolved_vector ( sources : std_logic_array_vector ) RETURN SiSP_Vector IS
+	VARIABLE result : SiSP_Vector; -- weakest state default
+	BEGIN
+		IF (sources'LENGTH = 1) THEN RETURN sources(sources'LOW);
+		ELSE
+			FOR i IN sources'RANGE LOOP
+				result := std_logic_vector(to_unsigned(2 ** (i - (sources'LENGTH + 1)),N) * unsigned(sources(i)));
+			END LOOP;
+			--result := result + sclk / (2 ** (sources'LENGTH + 1));
+		END IF;
+		RETURN result;
+	END resolved_vector;
 END PACKAGE BODY SiSP_Tools;
