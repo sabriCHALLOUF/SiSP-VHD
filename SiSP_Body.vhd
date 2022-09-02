ea54b07c1bff0ac8422eb66755f22a0cd462e822
Sabri <36789247+sabriCHALLOUF@users.noreply.github.com>
2022-09-02 11:11:12 +0100

    Update SiSP_Body.vhd

--- SiSP_Body.vhd
+++ SiSP_Body.vhd
@@ -2,7 +2,9 @@
 -- Author        : Sabri Challouf               --
 -- Creation date : 23 - 10 - 2016               --
 --                                              --
--- File description:                            --
+-- File description: This file contain the      --
+-- implementation of the behavior of the SiSP   --
+-- protocol 				        --
 --------------------------------------------------
 
 LIBRARY 	IEEE;
@@ -13,39 +15,32 @@ USE		WORK.SiSP_Tools.ALL;
 
 
 ENTITY SiSP_Body IS
-
-	PORT (
-				SBY_Clock		:IN		STD_LOGIC		;
-				SBY_Broadcast_In:IN		STD_LOGIC		;	
-				SBY_Rclk_in  	:IN		SiSP_Vector		;	                    
-				SBY_Sclk_out 	:OUT	SiSP_Vector		);	
-				
-			
-	SHARED	VARIABLE  Sclk: SiSP_Vector	:=	Full_0		;
-			
+	PORT (	SBY_Clock	:IN	STD_LOGIC	;
+		SBY_Broadcast_In:IN	STD_LOGIC	;	
+		SBY_Rclk_in  	:IN	SiSP_Vector	;	                    
+		SBY_Sclk_out 	:OUT	SiSP_Vector	);	
+	
+	SHARED	VARIABLE  Sclk: SiSP_Vector	:=	Full_0	;		
 END SiSP_Body; 
 
  
 ARCHITECTURE ARCH_SiSP_Body OF SiSP_Body IS
 BEGIN		
-								
 	-- Update Sclk with Rclk_In  & Increment Sclk
 	PROCESS(SBY_Clock , SBY_Rclk_in)
-		VARIABLE  	Sclk_Temp 		: SiSP_Vector	:= Full_0	;
-		VARIABLE	Edge_From_RclkIn: Boolean 		:= TRUE	 	;
+		VARIABLE  	Sclk_Temp : SiSP_Vector		:= Full_0;
+		VARIABLE	Edge_From_RclkIn: Boolean 	:= TRUE	 ;
 	BEGIN		
-	
 		IF (SBY_Clock = '0')
-		THEN	Edge_From_RclkIn 	:= TRUE	;
+		THEN	Edge_From_RclkIn := TRUE;
 		ELSE	IF (rising_edge(SBY_Clock))
 				THEN	IF (Sclk = Full_1) 
-						THEN Sclk := Full_0		;
+						THEN Sclk := Full_0	;
 						ELSE Sclk := Sclk + 1	;
 						END IF;
-						Edge_From_RclkIn 	:= FALSE	;
+						Edge_From_RclkIn := FALSE;
 				END IF;
 		END IF;
-				
 				
 		IF(Edge_From_RclkIn = TRUE)
 		THEN	IF (SBY_Rclk_in /= Unknown) and (SBY_Rclk_in /= UnInit)
@@ -54,7 +49,7 @@ BEGIN
 					Sclk_Temp 	:= Sclk ;
 					Sclk_Temp 	:= std_logic_vector(unsigned(Sclk_Temp) + unsigned(SBY_Rclk_in));
 					Sclk_Temp 	:= std_logic_vector(to_unsigned(0,Sclk_Temp'length)) + Sclk_Temp(N-1 downto 1);
-					Sclk 			:= Sclk_Temp ;
+					Sclk		:= Sclk_Temp ;
 				END IF;
 		END IF;
 	
@@ -63,7 +58,7 @@ BEGIN
 	-- Broadcast Sclk
 	PROCESS (SBY_Broadcast_In)
 	BEGIN
-			SBY_Sclk_out <= Sclk ;
+		SBY_Sclk_out <= Sclk ;
 	END PROCESS; 
 	
 END ARCH_SiSP_Body ; 
